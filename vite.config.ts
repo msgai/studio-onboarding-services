37da5431e18efb6bd15a95f92360fcd6a4a72ba8
Amit Chambial <amit.chambial@netomi.com>
2024-02-27 17:35:41 +0530

    asset dir changed

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   build: {
-    assetsDir: 'static',
+    assetsDir: 'onboarding_static',
   },
   resolve: {
     alias: {
