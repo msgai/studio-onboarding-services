7dbd110213ed6d2d4a49c3c64627c0e849b9b27e
Amit Chambial <amit.chambial@netomi.com>
2024-02-28 12:03:18 +0530

    fixed css

--- src/components/atoms/icon-uploader.tsx
+++ src/components/atoms/icon-uploader.tsx
@@ -43,14 +43,14 @@ const IconUploader: FunctionComponent<Props> = ({ value, onChange }) => {
   } else if(value){
     compLeft = <img height={50} width={50} src={value}/>
   } else {
-    compLeft = <>
+    compLeft = <div>
     <p className="text-base font-normal text-neutral-400">Choose a file or drag & drop it here</p>
     <p className="text-xs font-normal text-gray-400">IMG, JPG, JPEG format, up to 2MB</p>
-    </>
+    </div>
   }
   return (<div className="rounded-lg bg-white p-[24px]">
   <label htmlFor="dropzone-file" className="flex items-center cursor-pointer hover:bg-gray-100 justify-between rounded-lg border border-dashed border-neutral-900 px-[20px]">
-      <div className="flex flex-col items-center justify-center pt_5 pb_6">
+      <div className="flex flex-col items-center justify-center pt-[1.25rem] pb-[1.5rem]">
           {/* <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
           </svg> */}
